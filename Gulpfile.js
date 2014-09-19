ff3e18c2e46476aff3168386e4a5d1498b88d159
Peter Lejeck <peter.lejeck@gmail.com>
2014-09-19 15:44:09 -0700

    Huge improvements to build system

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var gulp = require('gulp');
 var esnext = require('gulp-esnext');
 var es6moduleTranspiler = require('gulp-es6-module-transpiler');
@@ -5,14 +6,16 @@ var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var bowerFiles = require('main-bower-files');
 var mergeStream = require('merge-stream');
+var debug = require('gulp-debug');
 var del = require('del');
-
 var paths = {
 	scripts: ['src/js/**/*'],
 	styles: ['src/css/app.less'],
-	images: ['src/img/**/*']
+	images: ['src/img/**/*'],
+	pages: ['**/*.htm']
 };
 
 gulp.task('clean', function(cb) {
@@ -20,7 +23,7 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('scripts', ['clean'], function () {
-	var scripts = gulp.src(paths.scripts)
+	return gulp.src(paths.scripts)
 		.pipe(sourcemaps.init())
 		.pipe(esnext())
 		.pipe(es6moduleTranspiler({
@@ -28,12 +31,46 @@ gulp.task('scripts', ['clean'], function () {
 		}))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('build/js'));
-	var bower = gulp.src(bowerFiles())
-		.pipe(sourcemaps.init())
-		.pipe(concat('vendor.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('build/js'));
-
-	return mergeStream(bower, scripts);
 });
+
+function vendorTree (tree) {
+	var tree = require(tree);
+	var trees = [];
+
+	for (var name in tree) {
+		if (tree.hasOwnProperty(name)) {
+		(function (name, pkg) {
+			var rel = function (p) { return path.join('bower_components', name, p); };
+			if (pkg.main) {
+				trees.push(gulp.src(rel(pkg.main))
+					.pipe(rename({basename: name, dirname: ""})));
+			}
+			if (pkg.more) {
+				trees.push(gulp.src(rel(pkg.more))
+					.pipe(rename(function (f) {
+						f.dirname = path.join(name, f.dirname);
+					})));
+			}
+		})(name, tree[name]);
+		}
+	}
+	return mergeStream.apply(null, trees);
+}
+
+gulp.task('vendor', ['clean'], function () {
+	return vendorTree('./vendor.json')
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(rename(function (f) {
+			f.dirname = f.dirname.replace('-amd', '')
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/js/vendor'));
+});
+
+gulp.task('pages', ['clean'], function () {
+	return gulp.src(paths.pages)
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['scripts', 'vendor', 'pages']);
