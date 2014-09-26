0560d3f1b6fb85ff94bcc032113e969048c87105
Peter Lejeck <peter.lejeck@gmail.com>
2014-09-25 22:32:21 -0700

    Add image and style tasks for Gulp + watch/livereload in development

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,8 @@ var path     = require('path');
 var gulp     = require('gulp');
 var del      = require('del');
 var lazypipe = require('lazypipe');
+// Fix Pipes
+var plumber = require('gulp-plumber');
 // General
 var gulpif      = require('gulp-if');
 var sourcemaps  = require('gulp-sourcemaps');
@@ -10,6 +12,10 @@ var concat      = require('gulp-concat');
 var rename      = require('gulp-rename');
 var bowerFiles  = require('main-bower-files');
 var mergeStream = require('merge-stream');
+// Watching
+var watch      = require('gulp-watch');
+var livereload = require('gulp-livereload');
+var notify     = require('gulp-notify');
 // JS
 var esnext    = require('gulp-esnext');
 var esmodules = require('gulp-es6-module-transpiler');
@@ -17,14 +23,18 @@ var uglify    = require('gulp-uglify');
 var jshint    = require('gulp-jshint');
 var defs      = require('gulp-defs');
 // CSS
-var less = require('gulp-less');
+var less         = require('gulp-less');
+var minifycss    = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+// Images
+var imagemin = require('imagemin');
 
 
 var paths = {
-	scripts: ['src/js/**/*'],
-	styles: ['src/css/app.less'],
+	scripts: ['src/js/**/*.js'],
+	styles: ['src/css/**/*.less'],
 	images: ['src/img/**/*'],
-	pages: ['**/*.htm']
+	pages: ['src/**/*.{htm,html}']
 };
 
 
@@ -57,9 +67,13 @@ function vendorTree (tree) {
 }
 
 function ifdev (fn) {
-	var args = Array.prototype.slice.call(arguments, 1);
 	return function () {
-		return gulpif(indev, fn.apply(null, args));
+		return gulpif(indev, fn.apply(null, arguments));
+	}
+}
+function ifnotdev (fn) {
+	return function () {
+		return gulpif(!indev, fn.apply(null, arguments));
 	}
 }
 
@@ -72,7 +86,7 @@ var es6 = lazypipe()
 	.pipe(esnext)
 	.pipe(esmodules, {type: 'amd'})
 	.pipe(defs, {disallowUnknownReferences: false})
-	.pipe(ifdev(uglify))
+	.pipe(ifnotdev(uglify))
 	.pipe(ifdev(sourcemaps.write));
 var hint = lazypipe()
 	.pipe(jshint, {debug: indev, devel: indev})
@@ -88,15 +102,20 @@ gulp.task('clean', function(cb) {
 
 gulp.task('scripts', function () {
 	return gulp.src(paths.scripts)
+		.pipe(plumber())
+		.pipe(ifdev(watch)(paths.scripts, {name: 'scripts'}))
 		.pipe(ifdev(hint)())
 		.pipe(es6())
-		.pipe(gulp.dest('build/js'));
+		.pipe(gulp.dest('build/js'))
+		.pipe(notify())
+		.pipe(ifdev(livereload)());
 });
 
 gulp.task('vendor', function () {
 	return vendorTree('./vendor.json')
+		.pipe(plumber())
 		.pipe(ifdev(sourcemaps.init)())
-		.pipe(ifdev(uglify)())
+		.pipe(uglify())
 		.pipe(rename(function (f) {
 			f.dirname = f.dirname.replace('-amd', '')
 		}))
@@ -104,9 +123,37 @@ gulp.task('vendor', function () {
 		.pipe(gulp.dest('build/js/vendor'));
 });
 
-gulp.task('pages', function () {
-	return gulp.src(paths.pages)
-		.pipe(gulp.dest('build'));
+gulp.task('styles', function () {
+	return gulp.src(paths.styles)
+		.pipe(plumber())
+		.pipe(ifdev(watch)(paths.styles, {name: 'styles'}))
+		.pipe(ifdev(sourcemaps.init)())
+		.pipe(less())
+		.pipe(autoprefixer())
+		.pipe(ifnotdev(minifycss)())
+		.pipe(ifdev(sourcemaps.write)())
+		.pipe(gulp.dest('build/css'))
+		.pipe(notify())
+		.pipe(ifdev(livereload)());
 });
 
-gulp.task('default', ['scripts', 'vendor', 'pages']);
+gulp.task('images', function () {
+	return gulp.src(paths.images)
+		.pipe(plumber())
+		.pipe(ifdev(watch)(paths.images, {name: 'images'}))
+		.pipe(gulp.dest('build/img'))
+		.pipe(notify())
+		.pipe(ifdev(livereload)());
+//		.pipe(imagemin())
+});
+
+gulp.task('pages', function () {
+	return gulp.src(paths.pages)
+		.pipe(plumber())
+		.pipe(ifdev(watch)(paths.pages, {name: 'pages'}))
+		.pipe(gulp.dest('build'))
+		.pipe(notify())
+		.pipe(ifdev(livereload)());
+});
+
+gulp.task('default', ['scripts', 'vendor', 'pages', 'styles', 'images']);
